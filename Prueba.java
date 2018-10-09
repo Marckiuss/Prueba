6e90a290d10c077257515adf3e3974f7afc9085c
Marckiuss <cram.ferrero@gmail.com>
2018-10-09 20:27:05 +0200

    + Ciudad de residencia actual

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,5 @@
 public class Prueba{
  public static void main(String args[]){
-  System.out.println("Marc Ferrero Fernandez\nLleida");
+  System.out.println("Marc Ferrero Fernandez\nLleida\nLeon");
  }
 }
